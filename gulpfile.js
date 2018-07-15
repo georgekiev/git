e82bae1c5b946acc1bfaee126f08b861de64c6e7
georgekiev <george.prokopenko@gmail.com>
2018-07-15 13:55:17 +0300

    testing

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+// var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+
+gulp.task('styles', function(){
+  gulp.src(['scss/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('css/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('app/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    // .pipe(jshint())
+    // .pipe(jshint.reporter('default'))
+    .pipe(babel())
+    
+    .pipe(gulp.dest('js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("scss/**/*.scss", ['styles']);
+  gulp.watch("app/**/*.js", ['scripts']);
+  gulp.watch("*.html", ['bs-reload']);
+});
